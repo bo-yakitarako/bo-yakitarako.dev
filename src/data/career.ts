0cdab1f8d2405973212fa25e5e1cae3b9042b359
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2026-03-07 08:08:00 +0900

    Careerを設定

--- src/data/career.ts
+++ src/data/career.ts
@@ -7,21 +7,64 @@ export interface CareerItem {
   section?: string;
 }
 
-// TODO: にじえもが実データを入れる
 export const careerItems: CareerItem[] = [
   {
     section: "学生時代",
-    period: "20XX.04 - 20XX.03",
-    title: "○○大学",
-    role: "情報工学科",
+    period: "2013/04 - 2016/03",
+    title: "宇都宮高校",
+    role: "普通科",
     type: "education",
   },
   {
-    section: "社会人",
-    period: "20XX.04 - 現在",
-    title: "株式会社サンプル",
+    period: "2017/04 - 2021/08",
+    title: "千葉大学",
+    role: "工学部 総合工学科 情報工学コース",
+    description: "退学",
+    type: "education",
+  },
+  {
+    section: "社員時代",
+    period: "2020/05 - 2023/02",
+    title: "株式会社SocialDog",
+    role: "React/PHPエンジニア",
+    description: "2021/09より契約社員",
+    type: "work",
+  },
+  {
+    section: "失業保険時代",
+    period: "2023/03 - 2023/09",
+    title: "フードデリバリー配達員",
+    role: "失業保険と配達員で生計",
+    description: "時折趣味開発を進める",
+    type: "work",
+  },
+  {
+    section: "業務委託時代",
+    period: "2023/10 - 2024/04",
+    title: "TechTech inc.(仮)",
+    role: "3件の小規模案件の開発",
+    description: "知り合いの紹介にて仕事を行いました",
+    type: "work",
+  },
+  {
+    period: "2024/05 - 2024/08",
+    title: "株式会社GNEX",
     role: "フロントエンドエンジニア",
-    description: "Web アプリケーション開発",
+    description: "自社プロダクトの開発",
+    type: "work",
+  },
+  {
+    period: "2024/09 - 2026/01",
+    title: "株式会社Palett",
+    role: "システム管理全般",
+    description: "GNEXからの紹介",
+    type: "work",
+  },
+  {
+    period: "2026/02 - 現在",
+    title: "株式会社Ceeglass",
+    role: "複数のアプリの開発全般",
+    description: "Palettからの紹介",
     type: "work",
   },
 ];
